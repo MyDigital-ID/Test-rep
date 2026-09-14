a2936d9300a437c1807bbee28bf2bdea6616604e
MyDigital-ID <rake2work@gmail.com>
2026-09-14 20:26:55 +0300

    Update script.js

--- Supermarket-Miami/script.js
+++ Supermarket-Miami/script.js
@@ -944,7 +944,7 @@ document.addEventListener('DOMContentLoaded', initApp);
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js?v=2')
+        navigator.serviceWorker.register('service-worker.js?v=16')
             .then(() => console.log('✅ Service Worker مسجّل'))
             .catch(e => console.log('❌ Service Worker:', e));
     });
